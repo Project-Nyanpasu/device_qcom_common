6b50a139a9ebf25be0f821ee5d4f579a50e8d79e
Jake Weinstein <xboxlover360@gmail.com>
2015-10-11 18:37:37 +0000

    common: imply certain variables for QC targets
    Change-Id: I6bbaf55c0c89d90dc55e77128d5f43e2ef88d62b

---
+++ common.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 Project ICE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_HAS_QC_KERNEL_SOURCE := true
+
+# Allow building audio encoders
+TARGET_USES_QCOM_MM_AUDIO := true
+
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/commonsys/cryptfs_hw
+endif
