52e304edba6287f9d17c00c871395440e4e2e43e
Alex Naidis <alex.naidis@linux.com>
2016-07-13 14:25:44 +0200

    common: add support for advanced DPM platform
    * add needed modules to bootclasspath
    * Target must provide these as packages!
    
    Change-Id: I09c69158269feecc2873848d05158fdce49be6e5
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- common.mk
+++ common.mk
@@ -31,6 +31,14 @@ PRODUCT_COPY_FILES += \
     device/qcom/common/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     device/qcom/common/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Advanced DPM
+ifeq ($(TARGET_WANTS_EXTENDED_DPM_PLATFORM),true)
+PRODUCT_BOOT_JARS += tcmclient
+PRODUCT_BOOT_JARS += com.qti.dpmframework
+PRODUCT_BOOT_JARS += dpmapi
+PRODUCT_BOOT_JARS += com.qti.location.sdk
+endif
+
 # QCOM HW crypto
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
     TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/commonsys/cryptfs_hw
