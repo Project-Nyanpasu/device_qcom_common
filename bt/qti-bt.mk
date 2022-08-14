0dcdff55a09c413d03cae8e8d0086406241f72c4
Jake Weinstein <jake@aospa.co>
2022-08-15 03:30:06 +0900

    common: bt: Build common packages
    Change-Id: I3abc190d78bc02b4ffa85eea9c8c6cfa590035a5

--- bt/qti-bt.mk
+++ bt/qti-bt.mk
@@ -18,6 +18,23 @@ include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)
 endif
 
+# Packages
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio@2.1-impl \
+    audio.bluetooth.default \
+    com.dsi.ant@1.0.vendor \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
+# FM
+ifeq ($(BOARD_HAVE_QCOM_FM),true)
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.fm@1.0.vendor
+endif
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
